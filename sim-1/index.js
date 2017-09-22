7345d7cf3c54f6cd8b9fec50676bd1ea349f68ed
Grant Dunn <pointyeyebrows@Grants-MacBook-Pro.local>
2017-09-22 11:59:56 -0600

    index-update

--- sim-1/index.js
+++ sim-1/index.js
@@ -1,7 +1,20 @@
-import express from "express";
-import bodyParser from 'body-parser';
-import massive from 'massive';
-import cors from 'cors';
+const express = require("express");
+const bodyParser = require('body-parser');
+const massive  = require('massive');
+const cors = require('cors');
+require('dotenv').config()
+
+const app = express();
+app.use( bodyParser.json());
+app.use( cors());
+massive( process.env.CONNECTION_STRING).then(db => app.set('db' , db ) );
+
+//endpoints
+
+const port = 3000
+app.listen(port, () => {console.log( `listening on port ${port}`);});
+
+
 
 
 
