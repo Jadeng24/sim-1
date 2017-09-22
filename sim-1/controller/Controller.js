eda38f6ae001e1409da89f50262ff04c440a2f7d
Grant Dunn <pointyeyebrows@Grants-MacBook-Pro.local>
2017-09-22 13:44:06 -0600

    2

---
+++ sim-1/controller/Controller.js
@@ -0,0 +1,17 @@
+module.exports = {
+    createBin: (req, res, next) => {
+
+    },
+    deletBin: (req, res, next) => {
+
+    },
+    getAllBins: (req, res, next) => {
+
+    },
+    getBin: (req, res, next) => {
+
+    },
+    updateBin: (req, res, next) => {
+
+    }
+}
