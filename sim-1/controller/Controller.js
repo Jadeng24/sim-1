536b8cfe24a5d25b2f6cbd99ee105ad5d306f489
Grant Dunn <pointyeyebrows@Grants-MacBook-Pro.local>
2017-09-22 14:22:09 -0600

    3

--- sim-1/controller/Controller.js
+++ sim-1/controller/Controller.js
@@ -1,17 +1,32 @@
 module.exports = {
     createBin: (req, res, next) => {
-
+        let db = req.app.get('db');
+        
     },
     deletBin: (req, res, next) => {
+        let db = req.app.get('db');
+        const { params } = req;
 
+        db.deletBin([parmas.id])
+        .then( () => res.status(200).send())
     },
     getAllBins: (req, res, next) => {
+        let db = req.app.get('db');
 
+        db.getAllBins().then(bins => res.status(200).send(bins));
     },
     getBin: (req, res, next) => {
+        let db = req.app.get('db');
+        const { params } = req;
 
+        db.getBin([parmas.id])
+        .then( () => res.status(200).send())
     },
     updateBin: (req, res, next) => {
-
+        let db = req.app.get('db');
+        const { params, queary}= req;
+        
+        updateBin.update_product([params.id, query.name, query.price])
+        .then( () => res.status(200).send() )
     }
 }
